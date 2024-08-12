a51af5317ffa21405ad9301cafd240b7f6469b82
Hamza-Elbouzidi <elbouzidihamza5@gmail.com>
2024-08-12 12:26:25 +0200

    changing the database schema and the structure of the prompt and the card data being passed

--- code/prompt.py
+++ code/prompt.py
@@ -2,20 +2,22 @@ import ollama
 
 output_format = 'JSON'
 
-def ask(requests, text, csv):
+def ask(json_structure, description, text, csv):
     query = (f"Vous êtes un modèle linguistique extrêmement précis et méticuleux chargé d'extraire des informations "
              f"spécifiques à partir de texte extrait par OCR. Le texte est fourni aux formats texte brut et CSV. "
-             f"Votre objectif est d'extraire uniquement les données suivantes : \n{requests}\n"
+             f"Votre objectif est d'extraire uniquement les données suivantes, en suivant la structure JSON fournie : \n{json_structure}\n"
+             f"Description : {description}\n"
              f"Ne rien extraire de plus ou de moins et ne pas ajouter de détails ou d'informations supplémentaires non demandées "
              f"à partir des fichiers suivants :\ncsv:\n{csv}\ntext:\n{text}\n"
              f"Vous devez extraire les données demandées et seulement celles demandées de manière concise et précise sous format "
-             f"{output_format}. Aucune donnée ou valeur ne doit être modifiée. Le résultat final doit être concis et "
+             f"{output_format}. Le résultat doit être un fichier JSON valide. Assurez-vous que le format JSON est correctement respecté. "
+             f"Aucune donnée ou valeur ne doit être modifiée. Le résultat final doit être concis et "
              f"ne contenir que l'information demandée, telle qu'elle est spécifiée dans les textes donnés. "
-             f"Si une donnée demandée n'est pas trouvée, indiquer explicitement 'introuvable'. "
+             f"Si une donnée demandée n'est pas trouvée, indiquez explicitement 'introuvable' en format JSON. "
              f"Ne pas mentionner vos connaissances linguistiques et ne pas ajouter de contexte. "
              f"Assurez-vous que les données extraites sont précises et correspondent exactement à celles présentes dans le texte.")
 
-    response = ollama.chat(model='llama3', messages=[{'role': 'user', 'content': query, },])
+    response = ollama.chat(model='llama3', messages=[{'role': 'user', 'content': query},])
 
     response = response['message']['content']
     return response
