e6b872b8948fef4af2bce6efd42fd0123c09b815
HAJAR ELFALLAKI-IDRISSI <149288694+Hajarfallaki@users.noreply.github.com>
2024-08-22 12:16:17 +0000

    update_interface_admin

--- code/app.py
+++ code/app.py
@@ -2,12 +2,12 @@ from flask import Flask, request, jsonify, render_template, redirect, url_for
 import os
 import shutil
 import magic
-import json
 from file_to_image import file_to_image
 from paddle_to_text import extract_text
 from paddle_to_csv import extract_csv
 from paddleocr import PaddleOCR
 from prompt import ask
+import json
 from db.db import db, Card
 
 parent_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +23,6 @@ os.makedirs(json_files_dir, exist_ok=True)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 ocr = PaddleOCR(lang='fr')
@@ -41,7 +40,7 @@ def get_images_paths():
     for d in os.listdir(converted_images_dir):
         d_path = os.path.join(converted_images_dir, d)
         if os.path.isfile(d_path) and is_image(d_path):
-            images_paths.append(d_path)
+                images_paths.append(d_path)
         else:
             for f in os.listdir(d_path):
                 f_path = os.path.join(d_path, f)
@@ -49,29 +48,62 @@ def get_images_paths():
                     images_paths.append(f_path)
     return images_paths
 
+def convert_type(value):
+    try:
+        value = value.replace(' ', '').replace(',', '.')
+        if '.' not in value:
+            return int(value)
+        return float(value)
+    except ValueError:
+        return value
+
+
 def save_json_file(text, image_path):
     json_name = f'{os.path.splitext(os.path.basename(image_path))[0]}'
     output_file_name = f"{json_name}.json"
     index = 1
     output_file_path = os.path.join(json_files_dir, output_file_name)
+
     while os.path.exists(output_file_path):
         output_file_name = f"{json_name}_{index}.json"
         output_file_path = os.path.join(json_files_dir, output_file_name)
         index += 1   
 
-    start_index = text.find('{')
-    end_index = text.rfind('}') + 1
+    try:
+        start_index = text.find('[')  # Start of JSON array
+        end_index = text.rfind(']') + 1  # End of JSON array
 
-    if start_index != -1 and end_index != -1:
-        json_content = text[start_index:end_index]
-        try:
-            data = json.loads(json_content)
-            with open(output_file_path, 'w') as file:
-                json.dump(data, file, indent=4)
-            return output_file_path
-        except json.JSONDecodeError:
-            return {}
-    return {}
+        if start_index != -1 and end_index != -1:
+            json_content = text[start_index:end_index]
+            app.logger.debug(f'Extracted JSON content: {json_content}')
+
+            try:
+                data = json.loads(json_content)
+                if not isinstance(data, list):
+                    app.logger.warning(f"Expected a JSON array but got: {data}")
+                    data = []
+
+                # Convert types in data
+                for item in data:
+                    for key, value in item.items():
+                        item[key] = convert_type(value)
+
+            except json.JSONDecodeError as e:
+                app.logger.error(f"Failed to decode JSON: {e}")
+                data = []  # Fallback to empty list
+        else:
+            app.logger.warning(f"No JSON array found in text: {text}")
+            data = []  # Fallback to empty list
+
+        with open(output_file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+        app.logger.info(f"JSON file saved: {output_file_path}")
+        return output_file_path
+    except Exception as e:
+        app.logger.error(f"Error saving JSON file: {e}")
+        return {}
+
+
 
 def load_json_data():
     json_data = {}
@@ -99,40 +131,26 @@ def delete_temp_files():
 def index():
     cards = Card.query.all()
     app.logger.debug('Cards retrieved: %s', cards)
-    return render_template('Admin_carte_page.html', cards=cards)
+    return render_template('index.html', cards=cards)
 
 @app.route('/admin/cards/add', methods=['POST'])
 def add_card():
-    title = request.form.get('title', '').strip()
-    json_structure = request.form.get('json_structure', '').strip()
-    description = request.form.get('description', '').strip()
-    
-    if not title or not json_structure:
-        return jsonify({'error': 'Title and JSON structure are required'}), 400
-
+    title = request.form['title']
+    json_structure = request.form['json_structure']
+    description = request.form['description']
     new_card = Card(title=title, json_structure=json_structure, description=description)
     db.session.add(new_card)
     db.session.commit()
-    return redirect(url_for('admin_carte_page'))
+    return redirect(url_for('admin_cards'))
 
 @app.route('/admin/cards/update/<int:card_id>', methods=['POST'])
 def update_card(card_id):
     card = Card.query.get(card_id)
-    if not card:
-        return jsonify({'error': 'Card not found'}), 404
-
-    title = request.form.get('title', '').strip()
-    json_structure = request.form.get('json_structure', '').strip()
-    description = request.form.get('description', '').strip()
-
-    if not title or not json_structure:
-        return jsonify({'error': 'Title and JSON structure are required'}), 400
-
-    card.title = title
-    card.json_structure = json_structure
-    card.description = description
+    card.title = request.form['title']
+    card.json_structure = request.form['json_structure']
+    card.description = request.form['description']
     db.session.commit()
-    return redirect(url_for('admin_carte_page'))
+    return redirect(url_for('admin_cards'))
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -140,33 +158,43 @@ def upload_file():
         print('File or cardData missing in request')
         return jsonify({'error': 'No files or card data provided'}), 400
 
-    files = request.files.getlist('files')
+    files = request.files.getlist('files')  # Get the list of files
     card_data_raw = request.form['cardData']
+    print(f"Raw card data: {card_data_raw}")  # Print the raw card data for debugging
 
     try:
+        # Decode the JSON-encoded string
         card_data = json.loads(card_data_raw)
-        if not isinstance(card_data, dict):
-            raise ValueError('Invalid card data format')
+        print(f"Parsed card data: {card_data}")  # Print the parsed card data for debugging
 
-        json_structure = card_data.get('json_structure', '{}')
-        description = card_data.get('description', '')
-    except (json.JSONDecodeError, ValueError) as e:
-        print(f"Error: {e}")
+        # Ensure card_data is a dictionary and extract values
+        if isinstance(card_data, dict):
+            json_structure = card_data.get('json_structure', '{}')
+            description = card_data.get('description', '')
+        else:
+            return jsonify({'error': 'Invalid card data format'}), 400
+    except json.JSONDecodeError as e:
+        print(f"JSONDecodeError: {e}")
         return jsonify({'error': 'Invalid card data format'}), 400
 
     if not files:
         return jsonify({'error': 'No files provided'}), 400
 
+    print(f'Received card data: {card_data}')
+
     for file in files:
+        print(f'Received file: {file.filename}')
+        # Save the uploaded file
         file_name = os.path.splitext(file.filename)[0]
         file_ext = os.path.splitext(file.filename)[1]
         file_path = os.path.join(uploads_dir, file.filename)
         i = 1
-        while os.path.exists(file_path):
+        if os.path.exists(file_path):
             file_path = os.path.join(uploads_dir, f'{file_name}_{i}{file_ext}')
             i += 1
         file.save(file_path)
 
+    # Convert documents to images
     file_to_image(uploads_dir, converted_images_dir)
 
     images_paths = get_images_paths()
@@ -175,16 +203,19 @@ def upload_file():
         text = extract_text(paddle_output)
         csv = extract_csv(paddle_output, image_path)
 
+        # Call the LLM with the correct parameters
         answer = ask(json_structure, description, text, csv)
+        print(answer)
         save_json_file(answer, image_path)
 
     delete_temp_files()
 
+    # Return a success message
     return jsonify({'message': 'Files processed and uploaded successfully!'}), 200
 
 @app.route('/preview')
 def preview():
-    json_data = load_json_data()
+    json_data = load_json_data()  # Load data for preview route
     return render_template('preview.html', json_data=json_data)
 
 @app.route('/Admin_login')
@@ -202,7 +233,7 @@ def admin_carte_page():
 
 @app.route('/preview/edit')
 def edit():
-    json_data = load_json_data()
+    json_data = load_json_data()  # Load data for edit route
     return render_template('edit.html', json_data=json_data)
 
 @app.route('/save', methods=['POST'])
--- code/static/carte_page.js
+++ code/static/carte_page.js
@@ -10,8 +10,132 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeCreateModalButton = document.getElementById('close-create-modal');
     const createCardButton = document.getElementById('btn1');  // Bouton de création de carte
     const cardsWrapper = document.querySelector('.cards-wrapper');
+    const cardContainer = document.getElementById('card-container');
+    const searchInput = document.getElementById('search-input');
+
 
     let selectedCard = null;
+
+    // Fonction pour nettoyer les données JSON
+    // function cleanJsonData(jsonData) {
+    //     return jsonData
+    //         .replace(/[{}]/g, '')   // Supprimer les accolades
+    //         .replace(/\\/g, '')     // Supprimer les barres obliques inverses
+    //         .replace(/"/g, '');     // Supprimer les guillemets doubles
+    //  }
+
+
+
+
+    // function cleanJsonData(jsonData) {
+    //     // Nettoyer la chaîne JSON en supprimant les accolades, barres obliques inverses, et guillemets doubles
+    //     let cleanedData = jsonData
+    //         .replace(/[{}]/g, '')   // Supprimer les accolades
+    //         .replace(/\\/g, '')     // Supprimer les barres obliques inverses
+    //         .replace(/"/g, '');     // Supprimer les guillemets doubles
+    
+    //     // Diviser la chaîne en parties
+    //     let parts = cleanedData.split(',');  // Diviser par les virgules pour obtenir chaque paire clé-valeur
+    //     let keys = [];
+    
+    //     // Extraire les clés
+    //     parts.forEach(part => {
+    //         let [key] = part.split(':');  // Diviser chaque partie par les deux-points
+    //         if (key) {
+    //             keys.push(key.trim());   // Ajouter la clé après avoir supprimé les espaces
+    //         }
+    //     });
+    
+    //     // Retourner uniquement les clés sous forme de chaîne, séparées par des virgules
+    //     return keys.join(', ');
+    // }
+
+
+    function cleanJsonData(jsonData) {
+        // Nettoyer la chaîne JSON en supprimant les accolades, barres obliques inverses, et guillemets doubles
+        let cleanedData = jsonData
+            .replace(/[{}]/g, '')   // Supprimer les accolades
+            .replace(/\\/g, '')     // Supprimer les barres obliques inverses
+            .replace(/"/g, '');     // Supprimer les guillemets doubles
+    
+        // Diviser la chaîne en parties
+        let parts = cleanedData.split(',');  // Diviser par les virgules pour obtenir chaque paire clé-valeur
+        let keys = [];
+    
+        // Extraire les clés
+        parts.forEach(part => {
+            let [key] = part.split(':');  // Diviser chaque partie par les deux-points
+            if (key) {
+                keys.push(key.trim());   // Ajouter la clé après avoir supprimé les espaces
+            }
+        });
+    
+        // Retourner uniquement les clés sous forme de chaîne, séparées par des retours à la ligne
+        return keys.join('\n');
+    }
+    
+
+
+
+    
+    // Fonction pour afficher une carte
+    function displayCard(cardData) {
+        const cardElement = document.createElement('div');
+        cardElement.className = 'card';
+        cardElement.dataset.cardTitle = cardData.title;
+        cardElement.dataset.cardDescription = cardData.description;
+        cardElement.dataset.cardJsonStructure = JSON.stringify(cardData.jsonStructure);
+
+        cardElement.innerHTML = `
+            <h3>${cardData.title}</h3>
+            <div class="card-details">
+                <p>${cardData.description}</p>
+                <pre>${JSON.stringify(cardData.jsonStructure, null, 2)}</pre>
+            </div>
+            <div class="button-container">
+                <button class="edit-button"><i class="bi bi-pencil-square"></i></button>
+                <button class="noisy-button"><i class="bi bi-eye"></i></button>
+                <button class="cancel-button" style="width:100px;">Supprimer</button>
+                <button class="validate-button">Valider</button>
+            </div>
+        `;
+
+        cardContainer.appendChild(cardElement);
+
+        // Ajouter l'événement de clic pour le bouton d'édition
+        cardElement.querySelector('.edit-button').addEventListener('click', (e) => {
+            e.stopPropagation();
+            selectedCard = cardElement;
+
+            // Récupérer les données JSON de la carte et les nettoyer
+            let rawJsonStructure = selectedCard.dataset.cardJsonStructure;
+            let cleanedKeys = cleanJsonData(rawJsonStructure);
+
+            // Remplir le formulaire de modification
+            document.getElementById('card-title').value = selectedCard.dataset.cardTitle;
+            document.getElementById('card-description').value = selectedCard.dataset.cardDescription;
+          document.getElementById('card-json-structure').value = cleanedKeys;
+
+            // Afficher la boîte modale
+            editCardModal.style.display = 'block';
+        });
+
+        // Ajouter les événements pour les boutons de suppression
+        cardElement.querySelector('.cancel-button').addEventListener('click', (e) => {
+            e.stopPropagation();
+            selectedCard = cardElement;
+            confirmDeleteDialog.style.display = 'block';
+        });
+
+        // Ajouter les événements pour les boutons de changement de couleur
+        cardElement.querySelector('.color-btn').addEventListener('click', (e) => {
+            e.stopPropagation();
+            const newColor = prompt('Entrez la nouvelle couleur en hex (ex. #FF5733) :');
+            if (newColor) {
+                cardElement.style.backgroundColor = newColor;
+            }
+        });
+    }
 
     // Fonction pour gérer les clics sur les boutons des cartes
     function setupCardButtons() {
@@ -33,25 +157,33 @@ document.addEventListener('DOMContentLoaded', () => {
                     // Remplir le formulaire avec les données de la carte
                     document.getElementById('card-title').value = selectedCard.dataset.cardTitle;
                     document.getElementById('card-description').value = selectedCard.dataset.cardDescription;
+                    document.getElementById('card-json-structure').value = cleanJsonData(selectedCard.dataset.cardJsonStructure);
                     // Afficher la boîte modale
                     editCardModal.style.display = 'block';
+                    document.getElementById('modal-backdrop').style.display = 'block'
                 } else {
                     console.log('Aucune carte sélectionnée');
                 }
             });
         });
 
-        // Gérer les boutons de changement de couleur
-        document.querySelectorAll('.card .color-btn').forEach(button => {
-            button.addEventListener('click', (e) => {
-                e.stopPropagation();
-                const card = e.target.closest('.card');
-                const newColor = prompt('Entrez la nouvelle couleur en hex (ex. #FF5733) :');
-                if (newColor) {
-                    card.style.backgroundColor = newColor;
-                }
-            });
+      // Gérer les boutons de changement de couleur
+      document.querySelectorAll('.card .noisy-button').forEach(button => {
+        button.addEventListener('click', () => {
+          if (confirm("Voulez-vous vraiment rendre la carte invisible par le user ?")) {
+            const cardElement = button.closest('.card');
+            cardElement.classList.add('transparent');
+            console.log(cardElement.classList); // Log the classList
+      
+            // Changement de l'icône
+            const iconElement = button.querySelector('i');
+            iconElement.classList.toggle('bi-eye');
+            iconElement.classList.toggle('bi-eye-slash');
+          }
         });
+      });
+        
+        
     }
 
     setupCardButtons();
@@ -72,7 +204,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Gérer la fermeture du formulaire modale
     closeEditModalButton.addEventListener('click', () => {
-        editCardModal.style.display = 'none';
+        editCardModal.style.display = 'none';     
+       document.getElementById('modal-backdrop').style.display = 'none';
+
     });
 
     // Gérer la soumission du formulaire d'édition
@@ -82,11 +216,16 @@ document.addEventListener('DOMContentLoaded', () => {
             // Mettre à jour les données de la carte avec les nouvelles valeurs
             selectedCard.dataset.cardTitle = document.getElementById('card-title').value;
             selectedCard.dataset.cardDescription = document.getElementById('card-description').value;
+            selectedCard.dataset.cardJsonStructure = document.getElementById('card-json-structure').value;
+
             // Mettre à jour le contenu de la carte affiché
             selectedCard.querySelector('h3').textContent = document.getElementById('card-title').value;
-            selectedCard.querySelector('p').textContent = document.getElementById('card-description').value;
+            selectedCard.querySelector('.card-details p').textContent = document.getElementById('card-description').value;
+            selectedCard.querySelector('.card-details pre').textContent = document.getElementById('card-json-structure').value;
+
             // Fermer la boîte modale
             editCardModal.style.display = 'none';
+            document.getElementById('modal-backdrop').style.display = 'none';
         } else {
             console.log('Aucune carte sélectionnée pour la mise à jour');
         }
@@ -95,11 +234,14 @@ document.addEventListener('DOMContentLoaded', () => {
     // Ouvrir la boîte modale de création de carte
     createCardButton.addEventListener('click', () => {
         createCardModal.style.display = 'block';
+        document.getElementById('modal-backdrop-create').style.display = 'block';
     });
 
     // Fermer la boîte modale de création de carte
     closeCreateModalButton.addEventListener('click', () => {
         createCardModal.style.display = 'none';
+        document.getElementById('modal-backdrop-create').style.display = 'none';
+
     });
 
     // Soumettre le formulaire de création de carte
@@ -109,50 +251,50 @@ document.addEventListener('DOMContentLoaded', () => {
         // Récupérer les valeurs du formulaire
         const newCardTitle = document.getElementById('new-card-title').value;
         const newCardDescription = document.getElementById('new-card-description').value;
+        const newCardJsonStructure = JSON.parse(document.getElementById('new-card-json-structure').value);
 
         // Créer une nouvelle carte
         const newCard = document.createElement('div');
         newCard.classList.add('card');
         newCard.dataset.cardTitle = newCardTitle;
         newCard.dataset.cardDescription = newCardDescription;
+        newCard.dataset.cardJsonStructure = JSON.stringify(newCardJsonStructure);
         newCard.innerHTML = `
             <h3>${newCardTitle}</h3>
-            <p>${newCardDescription}</p>
+            <div class="card-details">
+                <p>${newCardDescription}</p>
+                <pre>${JSON.stringify(newCardJsonStructure, null, 2)}</pre>
+            </div>
             <div class="button-container">
                 <button class="edit-button"><i class="bi bi-pencil-square"></i></button>
                 <button class="noisy-button"><i class="bi bi-eye"></i></button>
-                <button class="cancel-button" style="width:100px ;">Supprimer</button>
+                <button class="cancel-button" style="width:100px;">Supprimer</button>
                 <button class="validate-button">Valider</button>
             </div>
         `;
-        
 
-        // Ajouter la carte au conteneur des cartes
+        // Ajouter la nouvelle carte au conteneur
         cardsWrapper.appendChild(newCard);
 
-        // Réinitialiser le formulaire et fermer la modale
+        // Réinitialiser le formulaire et fermer la boîte modale
         createCardForm.reset();
         createCardModal.style.display = 'none';
 
-        // Reconfigurer les boutons des nouvelles cartes
+        // Réinitialiser les événements des boutons de la nouvelle carte
         setupCardButtons();
     });
-});
 
-const searchInput = document.getElementById('search-input');
-    const cards = document.querySelectorAll('.card');
-
-    searchInput.addEventListener('input', function() {
+    // Fonction de recherche
+    searchInput.addEventListener('input', () => {
         const searchValue = searchInput.value.toLowerCase();
-
-        cards.forEach(card => {
-            const title = card.getAttribute('data-card-title').toLowerCase();
-            const description = card.getAttribute('data-card-description').toLowerCase();
-
+        document.querySelectorAll('.card').forEach(card => {
+            const title = card.dataset.cardTitle.toLowerCase();
+            const description = card.dataset.cardDescription.toLowerCase();
             if (title.includes(searchValue) || description.includes(searchValue)) {
-                card.style.display = ''; // Affiche la carte
+                card.style.display = 'block';
             } else {
-                card.style.display = 'none'; // Cache la carte
+                card.style.display = 'none';
             }
         });
     });
+});
